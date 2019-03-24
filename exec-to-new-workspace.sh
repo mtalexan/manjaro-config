04244d462ae11428ce72a102d8349f48b5a6c345
Mike Alexander <mikealexander1860@gmail.com>
2019-03-23 20:52:45 -0700

    i3-easyfocus works.  Allow open on new workspace.  Browsers not floating. mod+9 is workspace 9

--- exec-to-new-workspace.sh
+++ exec-to-new-workspace.sh
@@ -9,8 +9,8 @@ SOURCE_DIR=$(dirname $(realpath -e "$0"))
 # Since we are starting this in the background on a new workspace,
 # keep it easy to find which workspace it was put on by putting it
 # on a new highest numbered workspace
-NEW_WORKSPACE_NUMBER=$(${SOURCE_DIR}/get-next-workspace-number.sh)
-CURRENT_WORKSPACE_NUMBER=$(${SOURCE_DIR}/get-current-workspace-number.sh)
+NEW_WORKSPACE_NUMBER=$(${SOURCE_DIR}/get-next-workspace-number.sh | tr -d '[[:space:]]')
+CURRENT_WORKSPACE_NUMBER=$(${SOURCE_DIR}/get-current-workspace-number.sh | tr -d '[[:space:]]')
 
 # jump to the new workspace, run the command, come back to this workspace, and print a notification
 i3-msg "workspace ${NEW_WORKSPACE_NUMBER} ; exec --no-startup-id \"$@\" ; workspace ${CURRENT_WORKSPACE_NUMBER}; exec --no-startup-id notify-send \"${NEW_WORKSPACE_NUMBER} : $@\"" &>/dev/null

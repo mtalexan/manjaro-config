1f341a1feeb6091183f8cdd7d587623d4242e479
Mike Alexander <mikealexander1860@gmail.com>
2019-12-13 21:03:33 -0800

    Moved config into subfolder, cleaned up dead scripts, updated config, added peco config

--- get-current-workspace-number.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Relies on i3-msg and jq
-
-# gets the workspaces, strips off the wrapping of the list syntax, finds
-# the focused one, and returns the number
-i3-msg -t get_workspaces | jq '.[] | select(.focused == true).num'
-
